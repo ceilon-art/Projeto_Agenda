ab90cc191e833407113e65ec6ac188cccc7c1c36
ceilon-art <andreluizkanaan222@gmail.com>
2021-03-31 11:01:51 -0300

    Filtered users

--- contatos/views.py
+++ contatos/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from .models import Contato
 from django.core.paginator import Paginator
 
 
 def index(request):
-    contatos = Contato.objects.all()
+    contatos = Contato.objects.order_by('-id').filter(
+        mostrar=True
+    )
     paginator = Paginator(contatos, 15)
 
     page = request.GET.get('p')
@@ -17,6 +20,10 @@ def index(request):
 
 def ver_contato(request, contato_id):
     contato = get_object_or_404(Contato, id=contato_id)
+
+    if not contato.mostrar:
+        raise Http404()
+
     return render(request, 'contatos/ver_contato.html', {
         'contato': contato
     })
